63b95ee0fd52a421b732e0e0b1575fee3056b8d7
abderrahim ghmouch <abderrahimghmouch47@gmail.com>
2026-01-28 15:45:19 +0100

    db connection

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,3 +1,4 @@
+-- Active: 1768971920114@@127.0.0.1@5432@linkup_db
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
         {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
@@ -10,10 +11,8 @@
         <div>
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
